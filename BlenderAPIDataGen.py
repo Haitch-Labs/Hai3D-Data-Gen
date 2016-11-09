f1b14e689ed720f84a2fd6446e064edd49ddb383
Jack Glendenning <jack.glendenning@gmail.com>
2016-11-09 11:34:24 -0800

    :sunglasses: Initial Commit of BlenderAPI Script

---
+++ BlenderAPIDataGen.py
@@ -0,0 +1,11 @@
+# Create Plain axis
+# Select the Camera and parent it to the main axis
+# Change the camera settings to have perpendicular view of plain axes
+# Rotate the Camera around the plain axis 360 degrees x10 in total of 100frames
+# Delete the Default Cube
+# Get the names of all our images
+# Import an image                             |
+# Center the image to fit the camera (ratio)  | - Repeated for each image in the dataset
+# Rotate the 3D model (in increments)         |
+# Render and save the image                   |
+
